9ce07f10f8517584e6a78dbbbfa31f34ad7be264
807462choi <807462@ais.edu.hk>
2017-05-28 20:52:37 +0800

    V. 2.3

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ canvas = 0;
 
 function setup()
 {
-  createCanvas(700,600);
+  createCanvas(800,600);
   triangleOrientation = 1;
   triangleOrientation2 = 2;
   triangleOrientation3 = 1;
@@ -115,69 +115,263 @@ function setup()
 
   moveGoldx = 220;
   moveGoldy = 150;
+  
+  canvas = intro;
 
 }
 
 function preload()
 {
   Gold = loadImage('https://dl.dropboxusercontent.com/s/x1mt1n2eqywabny/Coin2.png');  
-  IntroJ =  loadImage('https://dl.dropboxusercontent.com/s/adi59zn0kxdwjsd/Japan%20Castle.jpg');
+  IntroJ = loadImage('https://dl.dropboxusercontent.com/s/adi59zn0kxdwjsd/Japan%20Castle.jpg');
+  Prison = loadImage('https://dl.dropboxusercontent.com/s/1y1652mm8hmweh9/Prison.png');
+  PrisonO = loadImage('https://dl.dropboxusercontent.com/s/e3exhopjmrhy0x1/Outer%20Prison.png');
+  TeaR = loadImage('https://dl.dropboxusercontent.com/s/l9whvve9wke6b2l/Tea%20Room%202.0.png');
+  OuterGinshaw = loadImage('https://dl.dropboxusercontent.com/s/hvo1lnps14zokxo/Screen%20Shot%202017-05-15%20at%208.26.08%20AM.png');
+  Art = loadImage('https://dl.dropboxusercontent.com/s/xi78qie9woh1u9r/Screen%20Shot%202017-05-10%20at%208.11.16%20AM.png');
+  Scroll1 = loadImage('https://dl.dropboxusercontent.com/s/zfpsjwn5cej6kih/Screen%20Shot%202017-05-15%20at%208.35.45%20AM.png');
+  PrSgn = loadImage('https://dl.dropboxusercontent.com/s/20mrizcnbc56ali/Prison%20sign.png');
+  TeSgn = loadImage('https://dl.dropboxusercontent.com/s/tjahdskwzivrgx0/tearoom%20%281%29.png');
+  HaSgn = loadImage('https://dl.dropboxusercontent.com/s/a1omsr3277lt9oa/Hallway%20sign%20%281%29.png');
+  ArtSgn = loadImage('https://dl.dropboxusercontent.com/s/mempyrd7rmnrxq7/artroom%20v2%20%281%29.png');
+  Scrl = loadImage('https://dl.dropboxusercontent.com/s/1a0y1jbicgkqacc/Scroll%20%281%29.png');
+  Guitar = loadImage('https://dl.dropboxusercontent.com/s/pjjc43k7ckdo88d/Close%20up%20of%20the%20guitar.png');
+  GuitarF = loadImage('https://dl.dropboxusercontent.com/s/ltdc5r71s3lcyhw/Fretboard.jpg');
+  PuzzleS = loadImage('https://dl.dropboxusercontent.com/s/xmxcjqz8slm4a5m/Puzzle%20sign.png');
+    
+  
+  //MUSIC
+  IntroMus = loadSound('https://dl.dropboxusercontent.com/s/ye7qx0kuqicr8sm/Intro%20Music.mp3');
+  
+  PrisonMus = loadSound('https://dl.dropboxusercontent.com/s/ujdo6pmvknf04of/Prison.mp3');
+  
+  TeaRMus = loadSound("https://dl.dropboxusercontent.com/s/b46lq79zxcgsly2/Tea%20Room.mp3");
+  
+  //Sound FX
+  RockShamble = loadSound("https://dl.dropboxusercontent.com/s/yryang0hctpt9dk/Falling%20Rock%20Sound%20Effect.mp3");
+  
+  Clank = loadSound("https://dl.dropboxusercontent.com/s/xxpr5vgzlkma2rt/Cartoon%20Sound%20Effect%20-%20Metal%20Clank.mp3");
+  
+  
 }
 
 
 function draw()
 {
 
-  if (canvas == 1)
+  if (canvas == 1) // Prison
   {
     c1();
   }
-  else if (canvas == 2)
+  else if (canvas == 2) // puzzle
   {
     c2();
   }
-  else if (canvas == 3)
+  else if (canvas == 3) // outer prison
   {
     c3();
   }
+  else if (canvas == 4)// tea
+  {
+    c4();
+  }
+  else if (canvas == 5) //ART
+  {
+    c5();
+  }
+  else if (canvas == 6) //PRISON PUZZLE BROKEN
+  {
+    c6();
+  }
+  else if (canvas == 7) // Scroll
+  {
+    c7();
+  }
+  else if (canvas == 8) // Read Scroll
+  {
+    c8();
+  }
+  else if (canvas == 9) // sign prison
+  {
+    c9();
+  }
+  else if (canvas == 10) // prison room coin
+  {
+    c10();
+  }
+  else if (canvas == 11) // mandolin
+  {
+    c11();
+  }
+  else if (canvas == 12) // tea puzzle
+  {
+    c12();
+  }
+  else if (canvas == 13)
+  {
+    c13();
+  }
+  else if (canvas == 14)
+    {
+      c14();
+    }
   else
   {
     intro();
   }
-  
+
 }
 
 function intro()
 {
 
   background(50,150,150);
-  image(IntroJ,0,0,700,700);
+  image(IntroJ,0,-20,800,620);
+  if (IntroMus.isPlaying() == false)
+  {
+    IntroMus.play();
+  }
+
+  
   
   fill(255,0,0);
-  rect(250,300,200,50);
-  stroke(0,0,0);
-  text('START',250,250);
+  rect(280,300,200,50);
   
+  stroke(255,0,0);
+  fill(0,0,0);
+  textSize(50);
+  strokeWeight(5);
+  text('House of Ashikaga',170,250);
+ 
+  fill(0,0,0);
+  textSize(12);
+  strokeWeight(1);
+  text('START',360,330);
+  stroke(0,0,0);
 
-  if(mouseIsPressed)
+  if (mouseX >= 280 && mouseX <= 480 && mouseY >= 295 && mouseY <= 350)
   {
-    canvas = 1;
+    fill(155,0,0);
+    rect(280,300,200,50);
+    fill(0,0,0);
+    text('START',360,330);
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      canvas = 9;
+      IntroMus.stop();
+    }
+  }
+  else
+  {
+    cursor(ARROW);
   }
 }
 
 
 function c1()
 {
-  background(69,69,69);
-  if (mouseIsPressed)
+  image(Prison,0,-20,800,620);
+  if (PrisonMus.isPlaying() == false)
   {
-    canvas = 2;
+    PrisonMus.play();
   }
+
+  fill(255,0,0);
+  textSize(12);
+  
+  
+  var cursorimage = "arrow";
+
+  //puzzle
+  if (mouseX >= 10 && mouseX <= 70 && mouseY >= 470 && mouseY <= 500) 
+  {
+    cursorimage = "hand";
+    fill(255,255,255);
+    image(PuzzleS,150,70,500,375);
+    if (mouseIsPressed)
+    {
+      textSize(12);
+      canvas = 6;
+    }
+  }
+  
+  //Exit
+  if (mouseX >= 380 && mouseX <= 520 && mouseY >= 200 && mouseY <= 380 && goldatinv == 1)
+  {
+    cursorimage = "hand";
+    fill(255,255,255);
+    image(HaSgn,150,70,500,375);
+
+    if (mouseIsPressed)
+    {
+      goldatinv = 0;
+      canvas = 3;
+    }
+  }
+
+  
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+
+
+  
+  itemGrid();
+
 }
 
-function c2()
+function c9()
 {
-  background(255,255,255);         
+  image(Prison,0,-20,800,620);
+  image(PrSgn,0,0,800,600);
+  
+  textSize(30);
+  text("Continue",330,500);
+  
+  if (mouseX >= 330 && mouseX <= 450 && mouseY >= 480 && mouseY <= 600)
+    {
+      cursor(HAND);
+      if (mouseIsPressed)
+      {
+        canvas = 1;
+      }
+    }
+  else
+    {
+      cursor(ARROW);
+    }
+}
+
+
+
+function c6() // OUTER GINSHAW
+{
+ image(OuterGinshaw,0,0,800,600);
+  if (mouseX >= 100 && mouseX <= 700 && mouseY >= 50 && mouseY <= 500)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      RockShamble.play();
+      canvas = 2;
+    }
+  }
+  else 
+  {
+    cursor(ARROW);
+  }
+   
+}
+
+function c2() //puzzle
+{
+  background(255,255,255);
 
   //outline puzle 
   fill(204,102,0);
@@ -195,13 +389,11 @@ function c2()
   }
   drawTriangle1();
 
-
   if (permenantLock2 == false)
   {
     moveTriangle2();
     changeTriangleOrientation2();
   }
-
   drawTriangle2();
 
   if (permenantLock3 == false)
@@ -226,27 +418,313 @@ function c2()
   drawTriangle5();
   itemGrid();
   reward();
+  strokeWeight(1);
+}
+
+
+function c3() // outside prison
+{
+  image(PrisonO,0,0,800,600);
+  image(TeSgn,395,150,280,187);
+  if (mouseX >= 180 && mouseX <= 430 && mouseY >= 110 && mouseY <= 480)
+  {
+    cursor(HAND);
+    textSize(20);
+    fill(0,0,0);
+    //image(TeSgn,400,150,250,187);
+    if (mouseIsPressed)
+    {
+      canvas = 4;
+      cursor(ARROW);
+      PrisonMus.stop();
+    }
+  }
+  else
+  {
+    textSize(12);
+    cursor(ARROW);
+  }
+
+  itemGrid();
 
 }
 
-function c3()
+function c4() // tea room
 {
-  background(12,123,254);
+  image(TeaR,0,0,800,600);
+
+  var cursorimage = "arrow";
   
   
+  if (TeaRMus.isPlaying() == false)
+  {
+    TeaRMus.play();
+  }
+  
+  if (mouseX >= 0 && mouseX <= 70 && mouseY >= 150 && mouseY <= 510)
+  {
+    cursorimage = "hand";
+    textSize(20);
+    fill(0,0,0);
+    image(HaSgn,160,80,500,385);
+    if (mouseIsPressed)
+    {
+      canvas = 3;
+    }
+  }
+  else
+  {
+    textSize(12);
+  }
+  
+  if (mouseX >= 750 && mouseX <= 800 && mouseY >= 50 && mouseY <= 550)
+  {
+    cursorimage = "hand";
+    textSize(20);
+    fill(0,0,0);
+    image(ArtSgn,160,80,500,385);
+    if (mouseIsPressed)
+    {
+      cursorimage = "arrow";
+      TeaRMus.stop();
+      canvas = 5;
+    }
+  }
+  else
+  {
+    textSize(12);
+  }
+  
+  if (mouseX >= 600 && mouseX <= 660 && mouseY >= 400 && mouseY <= 450)
+  {
+    cursorimage = "hand";
+    fill(0,0,0);
+    textSize(20);
+    text('Evidence...?',350,250);
+    if (mouseIsPressed)
+    {
+      cursorimage = "arrow";
+      canvas = 7;
+    }
+  }
+  else
+  {
+    textSize(12);
+  }
+  
+  fill(0,0,0);
+  textSize(12);
+  
+  
+  rect(350,400,100,200);
+  
+  
+   
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+
   itemGrid();
 }
+
+function c7() //scroll
+{
+  var cursorimage = "arrow";
+  image(Scroll1,0,0,800,600);
+  fill(255,0,0);
+  rect(650,0,149,50);
+  fill(0,0,0);
+  textSize(20);
+  text('Back',700,35);
+
+  if (mouseX >= 150 && mouseX <= 550 && mouseY >= 100 && mouseY <= 500)
+  {
+    cursorimage = "hand";
+    if (mouseIsPressed)
+    {
+      cursorimage = "arrow";
+      canvas = 8;
+    }
+  }
+  
+  if (mouseX >= 650 && mouseX <= 800 && mouseY >= 0 && mouseY <= 50)
+  {
+    cursorimage = "hand";
+    if(mouseIsPressed)
+    {
+      cursorimage = "arrow";
+      canvas = 4;
+    }
+  }
+  
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+  
+}
+
+function c8() //read scroll
+{
+  image(Scrl,0,0,800,600);
+  fill(255,0,0);
+  rect(650,0,149,50);
+  fill(0,0,0);
+  textSize(20);
+  text('Back',725,35);
+  
+  if (mouseX >= 650 && mouseX <= 800 && mouseY >= 0 && mouseY <= 50)
+  {
+    cursor(HAND);
+    if(mouseIsPressed)
+    {
+      canvas = 4;
+    }
+  }
+  else
+  {
+    cursor(ARROW);
+  }
+}
+
+function c14() //tea puzzle
+{
+  background(255,255,255);
+  
+  
+  
+  
+}
+
+
+
+function c5() //Art room
+{
+  image(Art,0,-50,800,600);
+  var cursorimage = "arrow";
+  
+  if (mouseX >= 50 && mouseX <= 160 && mouseY >= 350 && mouseY <= 500)
+  {
+    cursorimage = "hand";
+    if (mouseIsPressed)
+      {
+        cursorimage = "arrow";
+        canvas = 10;
+      }
+  }
+  
+  rect(190,160,190,240);
+
+  
+  
+  if (mouseX >= 460 && mouseX <= 560 && mouseY >= 160 && mouseY <= 400)
+    {
+      cursorimage = "hand";
+      image(TeSgn,250,150,300,230);
+      if (mouseIsPressed)
+        {
+          cursorimage = "arrow";
+          canvas = 4;
+        }
+      
+    }
+  
+  
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+
+
+  itemGrid();
+}
+
+function c10() // guitar
+{
+  image(Guitar,0,0,800,600);
+  var cursorimage = "arrow";
+  
+  if (mouseX >= 250 && mouseX <= 450 && mouseY >= 150 && mouseY <= 520)
+    {
+      cursorimage = "hand";
+      if (mouseIsPressed)
+        {
+          canvas = 11; 
+        }
+    }
+  
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+}
+
+function c11()
+{
+  background(255,255,255);
+  image(GuitarF,0,100,1000,400);
+  var cursorimage = "arrow";
+  fill(255,0,0);
+  rect(660,0,120,50);
+  fill(0,0,0);
+  text('X',715,30);
+  
+  if (mouseX >= 660 && mouseX <= 780 && mouseY >= 0 && mouseY <= 120)
+    {
+      cursorimage = "hand";
+      if (mouseIsPressed)
+        {
+          cursorimage = "arrow";
+          canvas = 5;
+        }
+    }
+  
+  if (cursorimage == "arrow")
+  {
+    cursor(ARROW); 
+  }
+  else
+  {
+    cursor(HAND);
+  }
+  
+}
+
+
 
 function itemGrid()
 {
   fill(50,50,50);
-  rect(5,493,720,85);
+  rect(0,520,700,85);
   fill(125,125,125);
-  rect(20,500,70,70);
+  rect(10,525,70,70);
   fill(125,125,125);
-  rect(100,500,70,70);
+  rect(90,525,70,70);
   fill(125,125,125);
-  rect(180,500,70,70);
+  rect(170,525,70,70);
+  
+  if (goldatinv == 1)
+  {
+  image(Gold,moveGoldx,moveGoldy,goldsizex,goldsizey); 
+  }
+  
 }
 
 
@@ -544,8 +1022,6 @@ function changeTriangleOrientation5()
 
 }
 
-
-
 function mouseReleased()
 {
   if (lock)
@@ -629,7 +1105,6 @@ function mouseReleased()
   {
     lockTriangleOrientation5 = false;
   }
-
 }
 
 function drawTriangle1()
@@ -732,8 +1207,6 @@ function drawTriangle5()
   }
 }
 
-
-
 function maze()
 {
   stroke(0,0,0);
@@ -748,9 +1221,7 @@ function reward()
   if (lazy == 1)
   {
     //    GoldState = 0;
-    
-    
-    
+
     text(circleGold,10,100);
     image(Gold,moveGoldx,moveGoldy,goldsizex,goldsizey); 
     if (circleGold < 75)
@@ -758,11 +1229,12 @@ function reward()
       cursor(HAND);
       if (mouseIsPressed == true)
       {
-        moveGoldx = 30;
-        moveGoldy = 515;
+        moveGoldx = 20;
+        moveGoldy = 535;
         goldsizex = 50;
         goldsizey = 50;
         goldatinv = 1;
+        Clank.play();
       }
 
     }
@@ -771,9 +1243,10 @@ function reward()
       cursor(ARROW);
     }
   }
-  
+
   if (goldatinv == 1)
   {
+    Clank.stop();
     fill(255,50,50);
     if (mouseX >= 475 && mouseX <= 575 && mouseY >= 15 && mouseY <= 60)
     {
@@ -781,7 +1254,7 @@ function reward()
       cursor(HAND);
       if (mouseIsPressed)
       {
-        canvas = 3;
+        canvas = 1;
       }
     }
     else
@@ -790,10 +1263,8 @@ function reward()
     }
 
     rect(475,15,100,45);
-    
+
     fill(0,0,0);
     text("X",520,45);
   }
 }
-
-
